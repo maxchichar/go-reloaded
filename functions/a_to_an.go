94667bb58f91c1971ed69562c5ae9274a50293fd
maxchichar <maxwellchibuezecharles@gmail.com>
2026-04-15 04:39:56 +0100

    Handling 'a' e.g next = token[i+1]; if startWithVowel(next) {; token[i] = 'an'

--- functions/a_to_an.go
+++ functions/a_to_an.go
@@ -1,1 +1,43 @@
 package functions
+
+import (
+	"strings"
+)
+
+func ProcessAnRule(tokens []string) []string {
+	result := make([]string, 0, len(tokens))
+
+	for i := 0; i < len(tokens); i++{
+		current := tokens[i]
+
+		if isA(current) {
+			if i+1 < len(tokens) { // to check if there's a next word
+				next := tokens[i+1]
+				if startWithVowel(next) {
+					current = "an"
+				} else {
+					current = "a"
+				}
+				result = append(result, current)
+			}
+		}else {
+			result = append(result, current)
+		}
+	}
+	return result
+
+}
+
+func isA(token string) bool {
+	token = strings.ToLower(strings.TrimSpace(token))
+	return token == "a"
+}
+
+func startWithVowel(token string) bool {
+	if len(token) == 0{
+		return false
+	}
+
+	first := strings.ToLower(string(token[0]))
+	return strings.Contains("aeiouh", first)
+}
